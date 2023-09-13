d9bec6a3558ccff828178356bceed7522781149e
Joshua Guevara <joshguitar05@gmail.com>
2023-09-13 12:25:39 -0700

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -2,12 +2,18 @@ import './App.scss';
 import {Routes,Route} from 'react-router-dom'
 import Layout from './components/layout/Layout'
 import Home from './components/Home/Home';
+import About from './components/About/About';
+import Contact from './components/Contact/Contact';
 
 function App() {
   return (
    <Routes>
     <Route path='/' element={<Layout/>}>
       <Route index element={<Home/>}/>
+      <Route path='/about' element = {<About/>}/>
+      <Route path='/contact' element = {<Contact/>}/>
+
+
       </Route>
    </Routes>
   );
--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -5,19 +5,14 @@ import { Outlet } from 'react-router-dom'
 
 
 export default function Layout() {
-  console.log("one layout")
   return (
     <div className='App'>
       <Sidebar />
 
       <div className='page'>
-        <span className='tags-top-tags'>&lt;body&gt;</span>
+        {/* <span className='tags-top-tags'>&lt;body&gt;</span> */}
         <Outlet />
-        <span className='tags bottom-tags'>
-        &lt;body&gt;
-        <br/>
-        <span className = 'bottom-tag-html'>&lt;body&gt;</span>
-        </span>
+     
 
       </div>
     </div>
